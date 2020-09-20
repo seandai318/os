4b4166b3489498ca9938e569e35fdceedb7ef8b6
seandai318 <seandai318@gmail.com>
2020-09-20 15:00:08 -0500

    20200920 move osXmlParserIntf.h from xml to os folder

---
+++ include/osXmlParserIntf.h
@@ -0,0 +1,55 @@
+/* Copyright <c) 2020, Sean Dai
+ * xml parser interface
+ */
+
+#ifndef _OS_XML_PARSER_INTF_H
+#define _OS_XML_PARSER_INTF_H
+
+#include "osTypes.h"
+#include "osPL.h"
+#include "osMBuf.h"
+
+
+typedef enum {
+	OS_XML_DATA_TYPE_INVALID,
+	OS_XML_DATA_TYPE_XS_BOOLEAN,
+	OS_XML_DATA_TYPE_XS_UNSIGNED_BYTE,
+	OS_XML_DATA_TYPE_XS_SHORT,
+	OS_XML_DATA_TYPE_XS_INTEGER,
+	OS_XML_DATA_TYPE_XS_LONG,
+	OS_XML_DATA_TYPE_XS_STRING,	//xs:anyURI falls in this enum
+	OS_XML_DATA_TYPE_NO_XS,		//a collective type that represents  all no XS defined type, like simple and complex
+	OS_XML_DATA_TYPE_SIMPLE,
+	OS_XML_DATA_TYPE_COMPLEX,
+} osXmlDataType_e;
+
+
+
+typedef struct {
+    int eDataName;          //different app use different enum, like sipConfig_xmlDataName_e, diaConfig_xmlDataName_e, etc.
+    osPointerLen_t dataName;
+    osXmlDataType_e dataType;	 //for simpleType, this is set to OS_XML_DATA_TYPE_SIMPLE by user, and when callback, set to the XS type (like int, etc.)by the xmlparser
+    union {
+        bool xmlIsTrue;
+        uint64_t xmlInt;
+        osPointerLen_t xmlStr;
+    };
+} osXmlData_t;
+
+
+
+//app provided info for osXmlDataCallback_h callback
+typedef struct {
+	osXmlData_t* xmlData;
+	int maxXmlDataSize;
+} osXmlDataCallbackInfo_t;
+
+
+
+//get xml leaf node value based on the xsd and xml files
+osStatus_e osXml_getLeafValue(char* fileFolder, char* xsdFileName, char* xmlFileName,  bool isUseDefault, osXmlDataCallbackInfo_t* callbackInfo);
+bool osXml_isXsdValid(osMBuf_t* pXsdBuf);
+bool osXml_isXmlValid(osMBuf_t* pXmlBuf, osMBuf_t* pXsdBuf, osXmlDataCallbackInfo_t* callbackInfo);
+
+
+#endif
